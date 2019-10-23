7475560cc338b5f1fbaba538f7cd7e4f30a3a6d5
Noemi Lengyel <lengyel.noemi@live.com>
2019-10-23 20:54:35 +0200

    feature added: you can mark a todo as completed

--- src/app/todos.ts
+++ src/app/todos.ts
@@ -1,14 +1,14 @@
 import { ToDo } from './todo';
 
 export const TODOS: ToDo[] = [
-  { id: 11, name: 'Do the dishes', isDone: true },
+  { id: 11, name: 'Do the dishes', isDone: false },
   { id: 12, name: 'Go shopping', isDone: true },
   { id: 13, name: 'Iron the clothes', isDone: true },
-  { id: 14, name: 'Study', isDone: true },
+  { id: 14, name: 'Study', isDone: false },
   { id: 15, name: 'Water the plants', isDone: false },
   { id: 16, name: 'Feed the dog', isDone: false },
   { id: 17, name: 'Do yoga', isDone: false },
-  { id: 18, name: 'Meditate', isDone: false },
+  { id: 18, name: 'Meditate', isDone: true },
   { id: 19, name: 'Cook dinner', isDone: false },
   { id: 20, name: 'Workout', isDone: false }
 ];
