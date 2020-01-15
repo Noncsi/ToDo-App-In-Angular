aee10bf8149b8c195f804531f880c141349a26b0
unknown <lengyel.noemi96@gmail.com>
2020-01-15 12:36:35 +0100

    add next function, unsubscribings,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
@@ -8,9 +8,12 @@ const routes: Routes = [
   { path: 'list', component: ListComponent },
   { path: 'list/:id', component: TodoDetailComponent }
 ];
+const extra: ExtraOptions = {
+  enableTracing: true
+}
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, extra)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { TodoService } from '../todo.service';
 import { ToDo } from '../todo';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo-detail',
@@ -9,19 +11,33 @@ import { ToDo } from '../todo';
   styleUrls: ['./todo-detail.component.css']
 })
 export class TodoDetailComponent implements OnInit {
+  private ngUnsubscribe = new Subject<void>();
 
-  @Input() todo: ToDo;
+  todo: ToDo;
+  todoId: number
 
-  constructor(private todoService: TodoService, 
-              private route: ActivatedRoute) { }
+  constructor(private todoService: TodoService,
+    private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit() : void {
-    this.getTodo();
+  ngOnInit(): void {
+
+    this.route.params
+      .pipe(takeUntil(this.ngUnsubscribe.asObservable()))
+      .subscribe((value: Params) => {
+        this.todoId = +value['id'];
+        this.getTodo();
+      })
   }
 
   getTodo(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.todoService.getTodo(id)
-        .subscribe(data => this.todo = data);
+    this.todoService.getTodo(this.todoId)
+      .subscribe(data => this.todo = data);
+  }
+  next() {
+    this.router.navigate(['/list', ++this.todoId]);
+  }
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
